4125a8c38de4a80e888988fbdedf66ecdfc32185
jetbooster <jarvisam@gmail.com>
2019-08-25 16:13:23 +0100

    .

--- config.py
+++ config.py
@@ -6,6 +6,13 @@ util = rdr.util()
 
 util.debug = True
 
+DEFAULT_KEY = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+
+with open('./.keys') as fyl:
+  keyA = [hex(x) for x in bytearray(fyl.readline().strip().decode('hex'))]
+  keyB = [hex(x) for x in bytearray(fyl.readline().strip().decode('hex'))]
+
+
 while True:
   # Wait for tag
     rdr.wait_for_tag()
@@ -17,8 +24,11 @@ while True:
       if not error:
         print("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
         util.set_tag(uid)
-        util.auth(rdr.auth_b, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF])
-        for i in range(0,15):
-          util.write_trailer(i, )
+        util.auth(rdr.auth_b, DEFAULT_KEY)
+        for i in range(1,15):
+          util.write_trailer(i, key_a=keyA, key_b=keyB)
         util.dump()
         sleep(2)
+
+
+
