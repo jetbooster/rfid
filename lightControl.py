93d690586e584b074ef198ebfbf8fd1b389abc7a
jetbooster <jarvisam@gmail.com>
2019-08-25 14:53:24 +0100

    first push

---
+++ lightControl.py
@@ -0,0 +1,19 @@
+from pirc522 import RFID
+
+rdr = RFID()
+util = rdr.util()
+
+util.debug = True
+
+while True:
+  # Wait for tag
+    rdr.wait_for_tag()
+
+    # Request tag
+    (error, data) = rdr.request()
+    if not error:
+      (error, uid) = rdr.anticoll()
+      if not error:
+        print("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
+        util.set_tag(uid)
+        util.dump()
