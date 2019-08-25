639b879b7bcec64b3b797b248902c0af87156d6a
jetbooster <jarvisam@gmail.com>
2019-08-25 16:37:37 +0100

    .

--- lightControl.py
+++ lightControl.py
@@ -6,6 +6,10 @@ util = rdr.util()
 
 util.debug = True
 
+with open('./.keys') as fyl:
+  keyA = tuple([x for x in bytearray(fyl.readline().strip().decode('hex'))])
+  keyB = tuple([x for x in bytearray(fyl.readline().strip().decode('hex'))])
+
 while True:
   # Wait for tag
     rdr.wait_for_tag()
@@ -17,7 +21,7 @@ while True:
       if not error:
         print("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
         util.set_tag(uid)
-        util.auth(rdr.auth_b, [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF])
+        util.auth(rdr.auth_b, list(keyB))
         for i in range(0,15):
           util.write_trailer(i, )
         util.dump()
