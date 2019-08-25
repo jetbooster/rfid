c71af1dca2f246712038e35b5cf49c13bd98de9a
jetbooster <jarvisam@gmail.com>
2019-08-25 18:04:18 +0100

    .

--- lifxControl.py
+++ lifxControl.py
@@ -1,12 +1,15 @@
 from lifxlan import LifxLAN
 
-class lifx(LifxLAN):
+class Lifx(LifxLAN):
   def __init__(self,*args,**kwargs):
     super().__init__(args, kwargs)
 
     self.bulb = self.getLights()[0]
 
     
-  def toggle_power(self, interval=0.5):
+  def toggle_power(self):
       original_power_state = self.bulb.get_power()
       self.bulb.set_power("on" if original_power_state==0 else "off")
+
+lifx = Lifx(1)
+lifx.toggle_power()
