b0de59edd1ebf6d4e83f9935c9638130f02af684
jetbooster <jarvisam@gmail.com>
2019-08-25 16:52:45 +0100

    .

--- lightControl.py
+++ lightControl.py
@@ -23,6 +23,6 @@ while True:
       if not error:
         print("Card read UID: "+str(uid[0])+","+str(uid[1])+","+str(uid[2])+","+str(uid[3]))
         util.set_tag(uid)
-        util.auth(rdr.auth_b, list(keyB))
+        util.auth(rdr.auth_a, list(keyA))
         util.dump()
         sleep(2)
